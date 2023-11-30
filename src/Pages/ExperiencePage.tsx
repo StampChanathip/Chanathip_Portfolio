b082b5f7b7b2755e6d12b1995495fb71159147fe
StampChanathip <stamp1234561@gmail.com>
2023-11-30 23:59:45 +0700

    Fixed Image not showing

--- src/Pages/ExperiencePage.tsx
+++ src/Pages/ExperiencePage.tsx
@@ -10,7 +10,7 @@ const ExperiencePage = () => {
         title="Full Stack Developer Internship"
         place="Geotalent Co., Ltd."
         period="29 June 2023 – 11 August 2023"
-        imgUrl="Assets/Intern.jpg"
+        imgUrl="Assets/Intern.JPG"
         details="Get fundamental training in web development and join a team to work as a full-stack developer developing the user interface for contract work for the company."
       />
       <ExperienceCard
@@ -24,14 +24,14 @@ const ExperiencePage = () => {
         title="Head of Event Organizer"
         place="Chula Investment Forum"
         period="20 June 2022 – 10 September 2022"
-        imgUrl="Assets/Investment.jpg"
+        imgUrl="Assets/Investment.JPG"
         details="Establish and clarify the project's goals and scope. Manage the organizing team to plan the event, which drew a total of 160 attendees."
       />
       <ExperienceCard
         title="Bitkub Academy Blockathon Boot Camp Participant"
         place="Bitkub Labs Co., Ltd."
         period="15 November 2022 – 18 November 2022"
-        imgUrl="Assets/Bitkub.jpg"
+        imgUrl="Assets/Bitkub.JPG"
         details="Participate in business pitching competition during Bootcamp and responsible for technical research role in business pitching competition"
       />
       <ExperienceCard
